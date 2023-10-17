a34e9877ceec480ee9cba1d0b3715652202eec43
j4ck3 <jacobhallgren@live.se>
2023-10-18 00:20:28 +0200

    Skapa en 50% off del.

---
+++ Manero_WebApp/ViewModels/HomeViewModels/ImageSliderViewModel.cs
@@ -0,0 +1,7 @@
+﻿namespace Manero_WebApp.ViewModels.HomeViewModels;
+public class ImageSliderViewModel
+{
+    public string? Title1 { get; set; }
+    public string? Title2 { get; set; }
+    public string ImageUrl { get; set; } = null!;
+}
