6bb6553408790670145331d9574d9d98d6999453
Hawa <111865602+Monstermera@users.noreply.github.com>
2023-10-17 08:08:05 +0200

    Folders added and packages installed

--- Manero_WebApp/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Manero_WebApp.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
