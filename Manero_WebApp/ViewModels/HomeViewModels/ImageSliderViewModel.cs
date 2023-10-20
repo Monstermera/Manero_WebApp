0b4a7a97387168aca24d14e97d775d2438ade88f
Elias <94994473+EliasAlmen@users.noreply.github.com>
2023-10-20 10:41:50 +0200

    Revert "Home page partials"

--- Manero_WebApp/ViewModels/HomeViewModels/ImageSliderViewModel.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Manero_WebApp.ViewModels.HomeViewModels;
-public class ImageSliderViewModel
-{
-    public string? Title1 { get; set; }
-    public string? Title2 { get; set; }
-    public string ImageUrl { get; set; } = null!;
-}
