8982ed6d4575ed3f7df8f4bef749e3483b11475b
Erucio <111879095+Erucio@users.noreply.github.com>
2023-10-24 17:06:54 +0200

    Började med Rollhantering, admin och customer tillsammans med Oscar

--- Manero_WebApp/Helpers/Services/AuthenticationServices/RegisterService.cs
+++ Manero_WebApp/Helpers/Services/AuthenticationServices/RegisterService.cs
@@ -3,16 +3,19 @@ using Manero_WebApp.Models.Entities;
 using Manero_WebApp.Models.Schemas;
 using Manero_WebApp.ViewModels.AccountViewModels;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 
 namespace Manero_WebApp.Helpers.Services.AuthenticationServices;
 
 public class RegisterService
 {
     private readonly UserManager<UserEntity> _userManager;
+    private readonly RolesService _rolesService;
 
-    public RegisterService(UserManager<UserEntity> userManager)
+    public RegisterService(UserManager<UserEntity> userManager, RolesService rolesService)
     {
         _userManager = userManager;
+        _rolesService = rolesService;
     }
 
     public async Task<bool> RegisterAsync(RegistrationViewModel registrationViewModel)
@@ -22,6 +25,7 @@ public class RegisterService
         var result = await _userManager.CreateAsync(user, registrationViewModel.Password);
         if (result.Succeeded)
         {
+            var test = await _rolesService.AddRoleAsync(registrationViewModel);
             return true;
         }
         return false;
---
+++ Manero_WebApp/Helpers/Services/AuthenticationServices/RolesService.cs
@@ -0,0 +1,46 @@
+﻿using Manero_WebApp.Models.Entities;
+using Manero_WebApp.ViewModels.AccountViewModels;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+namespace Manero_WebApp.Helpers.Services.AuthenticationServices;
+
+public class RolesService 
+{
+    private readonly UserManager<UserEntity> _userManager;
+    private readonly RoleManager<IdentityRole> _roleManager;
+
+    public RolesService(UserManager<UserEntity> userManager, RoleManager<IdentityRole> roleManager)
+    {
+        _userManager = userManager;
+        _roleManager = roleManager;
+    }
+
+    public async Task<UserEntity> AddRoleAsync(UserEntity userModel)
+    {
+        //UserEntity user = viewModel;
+        string roleName = "customer";
+
+        if (!await _roleManager.Roles.AnyAsync())
+        {
+            await _roleManager.CreateAsync(new IdentityRole("admin"));
+            await _roleManager.CreateAsync(new IdentityRole("customer"));
+
+        }
+
+        if (!await _userManager.Users.AnyAsync())
+        {
+            roleName = "admin";
+
+        }
+        else roleName = "customer";
+
+        IdentityResult roleresult = await _userManager.AddToRoleAsync(userModel, roleName);
+        //await _userManager.AddToRoleAsync(user, roleName);
+        return userModel;
+        
+    }
+
+
+
+}
