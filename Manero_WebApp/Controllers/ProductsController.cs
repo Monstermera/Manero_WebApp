dbe661643abddbaed0852ad67203ac543d1c3407
Oscar Nilsson <o.nilsson743@gmail.com>
2023-10-31 15:11:02 +0100

    Edited Edit page and fixed update function

--- Manero_WebApp/Controllers/ProductsController.cs
+++ Manero_WebApp/Controllers/ProductsController.cs
@@ -29,52 +29,30 @@ public class ProductsController : Controller
     [HttpGet]
     public async Task<IActionResult> Edit(Guid Id)
     {
-        ProductModel product;
-        if (Id != Guid.Empty)
-        {
-           product = await _productService.GetAsync(Id);
-        }
-        else
-        {
-            product = await _productService.GetAsync(Guid.Parse("6f0cfb0b-053b-4e69-b485-323e827659f1"));
-        }       
-        
+        ProductModel product = await _productService.GetAsync(Id);      
 
         ViewBag.Tags = await _productService.GetProductPropertiesAsync(_context.Tags, tag => tag.TagName, tag => tag.Id.ToString());
         ViewBag.Sizes = await _productService.GetProductPropertiesAsync(_context.Sizes, size => size.SizeName, size => size.Id.ToString());
         ViewBag.Colors = await _productService.GetProductPropertiesAsync(_context.Colors, color => color.ColorName, color => color.Id.ToString());
         ViewBag.Categories = await _productService.GetProductPropertiesAsync(_context.Categories, category => category.CategoryName, category => category.Id.ToString());
+       
         return View(product);
     }
 
     [HttpPost]
     public async Task<IActionResult> Edit(ProductModel updatedProduct)
     {
-        //var updatedProduct = new ProductModel
-        //{
-        //    ArticleNumber = Guid.Parse("6f0cfb0b-053b-4e69-b485-323e827659f1"),
-        //    Name = "Test update1",
-        //    Price = 299,
-        //    Description = "Update again",
-        //    AmountInStock = 90,
-        //    Colors = new List<string> { "red" },
-        //    Sizes = new List<string> { "m", "l" },
-        //    //Tags = new List<string> { "sale" },
-        //    //Categories = new List<string> { "red" },
-        //    //Colors = new List<string> { "red" },
-        //    //Colors = new List<string> { "red" },
-
-
-        //};
-
         var result = await _updateProductService.UpdateAsync(updatedProduct);
+       
         if (result != null)
         {
             ViewBag.Tags = await _productService.GetProductPropertiesAsync(_context.Tags, tag => tag.TagName, tag => tag.Id.ToString());
             ViewBag.Sizes = await _productService.GetProductPropertiesAsync(_context.Sizes, size => size.SizeName, size => size.Id.ToString());
             ViewBag.Colors = await _productService.GetProductPropertiesAsync(_context.Colors, color => color.ColorName, color => color.Id.ToString());
             ViewBag.Categories = await _productService.GetProductPropertiesAsync(_context.Categories, category => category.CategoryName, category => category.Id.ToString());
+            
             ProductModel model = result;
+           
             return View(model);
         }
 
