0fd841c9a3e5a2616f4f0db2183b1bbd8dad0025
dimetcicek <dimet.cicek96@hotmail.com>
2023-11-09 20:24:32 +0100

    added add to wishlist functionality

---
+++ Manero_WebApp.tests/EnhetsTest/WishlistController_Tests.cs
@@ -0,0 +1,57 @@
+﻿using Manero_WebApp.Controllers;
+using Manero_WebApp.Helpers.Repositories;
+using Manero_WebApp.Helpers.Services.ProductServices;
+using Manero_WebApp.Models.Schemas;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Manero_WebApp.Tests.EnhetsTest
+{
+    /// <summary>
+    /// TESTER PÅ WISHLISTCONTROLLER AV DIMET
+    /// </summary>
+    public class WishlistController_Tests
+    {
+        private readonly Mock<WishlistRepo> _mockWishlistRepo;
+        private readonly Mock<IGetOneProductService> _mockGetOneProductService;
+        private readonly Mock<WishlistProductService> _mockWishlistProductService;
+        private readonly Mock<HttpContext> _mockHttpContext;
+        private readonly WishlistController _controller;
+
+        public WishlistController_Tests()
+        {
+            _mockGetOneProductService = new Mock<IGetOneProductService>();
+            _mockWishlistRepo = new Mock<WishlistRepo>();
+            _mockWishlistProductService = new Mock<WishlistProductService>(_mockWishlistRepo.Object, _mockGetOneProductService.Object);
+            _mockHttpContext = new Mock<HttpContext>();
+
+            _controller = new WishlistController(_mockWishlistProductService.Object)
+            {
+                ControllerContext = new ControllerContext
+                {
+                    HttpContext = _mockHttpContext.Object
+                }
+            };
+        }
+
+        /// <summary>
+        /// Tests that adding a new product actually adds it to the wishlist.
+        /// </summary>
+        [Fact]
+        public async Task Add_NewProduct_AddsItToWishlist()
+        {
+            var fakeProduct = new ProductModel
+            { 
+                ArticleNumber = Guid.NewGuid()
+            };
+
+            _mockGetOneProductService.Setup(i => i.GetOneProductAsync(It.IsAny<Guid>())).ReturnsAsync(fakeProduct);
+
+            var response = await _controller.Add(fakeProduct.ArticleNumber);
+
+            var okResponse = Assert.IsType<OkObjectResult>(response);
+
+            Assert.Equal(fakeProduct.ArticleNumber, okResponse.Value);
+        }
+    }
+}
---
+++ Manero_WebApp/Helpers/Services/WishlistServices/WishlistProductService.cs
@@ -0,0 +1,55 @@
+﻿namespace Manero_WebApp.Helpers.Services.ProductServices;
+
+using Manero_WebApp.Helpers.Repositories;
+using Manero_WebApp.Models.Schemas;
+
+public class WishlistProductService
+{
+    private readonly WishlistRepo _wishlistRepo;
+    private readonly IGetOneProductService _getOneProductService;
+
+    public WishlistProductService(WishlistRepo wishlistRepo, IGetOneProductService getOneProductService)
+	{
+        _wishlistRepo = wishlistRepo;
+        _getOneProductService = getOneProductService;
+    }
+
+    public async Task<bool> Add(Guid articleNumber)
+    {
+        var product = await _getOneProductService.GetOneProductAsync(articleNumber);
+
+        return await Add(product);
+    }
+
+    public async Task<bool> Add(ProductModel model)
+	{
+		if (model != null)
+		{
+            return await _wishlistRepo.Add(model);
+        }
+
+        return false;
+    }
+
+    public async Task<bool> Remove(Guid articleNumber)
+    {
+        var product = await _getOneProductService.GetOneProductAsync(articleNumber);
+
+        return await Remove(product);
+    }
+
+    public async Task<bool> Remove(ProductModel model)
+    {
+        if (model != null)
+        {
+            return await _wishlistRepo.Remove(model);
+        }
+
+        return false;
+    }
+
+    public async Task<IEnumerable<ProductModel>> GetAll()
+    {
+        return await _wishlistRepo.GetAll();
+    }
+ }
