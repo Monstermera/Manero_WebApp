c58b83c75203d8c9d70a18e696f902dbe6a91c56
filipblyckert <112581641+filipblyckert@users.noreply.github.com>
2023-11-14 15:13:53 +0100

    Added forgot password. Fixed error loginview Added tests

---
+++ Manero_WebApp.tests/EnhetsTest/ForgotPasswordTests.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using Bunit;
+using System.ComponentModel;
+using Manero_WebApp.ViewModels.AccountViewModels;
+using System.ComponentModel.DataAnnotations;
+
+namespace Manero_WebApp.Tests.EnhetsTest
+{
+    public class ForgotPasswordTests : TestContext
+    {
+        private ForgotPasswordViewModel _viewModel;
+        [Fact]
+        public void ForgotPassword_ValidEmail_GoodValidation()
+        {
+            // Arrange 
+            _viewModel = new ForgotPasswordViewModel();
+            _viewModel.Email = "validemail@gmail.com";
+            var validationResults = new List<ValidationResult>();
+            var ctx = new ValidationContext(_viewModel, null, null);
+
+            // Act
+            Validator.TryValidateObject(_viewModel, ctx, validationResults, true);
+
+            // Assert
+            Assert.Empty(validationResults);
+        }
+
+        [Fact]
+        public void ForgotPassword_EmptyEmail_BadValidation()
+        {
+            // Arrange 
+            _viewModel = new ForgotPasswordViewModel();
+            var validationResults = new List<ValidationResult>();
+            var ctx = new ValidationContext(_viewModel, null, null);
+
+            // Act
+            Validator.TryValidateObject(_viewModel, ctx, validationResults, true);
+
+            // Assert
+            Assert.Single(validationResults);
+            Assert.Equal("You must provide an e-mail", validationResults[0].ErrorMessage );
+        }
+
+    }
+}
---
+++ Manero_WebApp/ViewModels/AccountViewModels/ForgotPasswordViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Manero_WebApp.ViewModels.AccountViewModels;
+
+public class ForgotPasswordViewModel
+{
+    [Display(Name = "E-Mail*")]
+    [Required(ErrorMessage = "You must provide an e-mail")]
+    public string Email { get; set; } = null!;
+
+
+}
